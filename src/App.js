4e4639267bb878b9acc30423e09e1e567502ea65
AlexMagar <lx.mgr07@gmail.com>
2024-01-01 20:48:40 +1100

    initial files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  // let  data = ['sdfljgsd,sdfgs,dfg,sdf,gsd,fgd']
+  
+  const ArrangeText = (data) => {
+    data &&
+    data.map((index, i) => {
+      return(
+        <span key={index}>{
+          (index ? ',' : " ") + data
+        }
+        </span>
+      )
+    })
+  }
+
   return (
     <div className="App">
-      
+      <ArrangeText text={'sdfljgsd,sdfgs,dfg,sdf,gsd,fgd'}/>
     </div>
   );
 }
